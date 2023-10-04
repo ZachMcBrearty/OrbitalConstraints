b25c40e5075eb7c978a7ff24d0063a81373887a2
ZachMcBrearty <mcbreartyzg@gmail.com>
2023-10-04 10:47:02 +0100

    switched to function derivative method in latitude branch second order central using 2nd neighbours is much more stable

--- OrbitalConstraints.py
+++ OrbitalConstraints.py
@@ -9,26 +9,42 @@
 # d/dx (D(1-x^2)*dT/dx)
 # expand outer derivative
 # d(D(1-x^2))/dx * dT/dx + D(1-x^2)* d^2(T)/ dx^2
+# diff = dD/dx (1-x^2) * dT/dx + D*-2x*dT/dx + D(1-x^2)* d^2(T)/ dx^2
 
-# discretise space derivatives by central difference:
-# dT/dx = (T(x_m+1, t_n) - T(x_m-1, t_n)) / 2Δx
-# d^2 T / dx^2 = (T(x_m+2, t_n) - 2*T(x_m, t_n) + T(x_m-2, t_n)) / 4 Δx^2
-# dD/dx = (D(x_m+1, t_n) - D(x_m-1, t_n)) / 2Δx
+# discretise space derivatives using derivative functions below
+# Forward and backward for edge cases
+# central otherwise
 
 # All together:
 # T(x_m, t_n+1) = T(x_m, t_n) + Δt / C(x_m, t_n)
-# * ((T(x_m+2)-2*T(x_m)+T(x_m-2))*D(x_m)(1-x_n^2)/4Δx^2
-#     + (T(x_m+1) - T(x_m-1)) * ((1-x^2_m)*(D(x_m+1)-D(x_m-1))/2Δx - 2x_m*D(x_m)) / 2Δx)
-# - I + S(1-A) )
-# i.e. a mess
-# Boundaries: wrap back / reflect to same latitude
+# * (diff - I + S(1-A) )
 
 import numpy as np
 
 from InsolationFunction import S
 
+## derivatives ##
+def forwarddifference(x, i, dx):
+    return (x[i+1] - x[i  ]) / dx
+def centraldifference(x, i, dx):
+    # (x[i+1/2] - x[i-1/2]) / dx
+    # let x[i+(-)1/2] = (x[i+(-)1] + x[i]) / 2
+    # => (x[i+1] - x[i-1]) / (2*dx)
+    return (x[i+1] - x[i-1]) / (2*dx)
+def backwarddifference(x, i, dx):
+    return (x[i  ] - x[i-1]) / dx
+
+def forward2ndorder(x,i,dx):
+    return (x[i+2] - 2*x[i+1] + x[i  ]) / dx / dx
+def central2ndorder(x,i,dx):
+    return (x[i+2] - 2*x[i  ] + x[i-2]) / (2*dx) / (2*dx)
+def backward2ndorder(x,i,dx):
+    return (x[i  ] - 2*x[i-1] + x[i-2]) / dx / dx
+
+##  ##
 spacedim = 200  # number of points in space
 dx = 2 / (spacedim - 1)  # spacial separation from 2 units from -1 to 1
+xs = np.linspace(-1, 1, spacedim)
 
 # timedim may need to be split in to chunks which are then recorded
 # e.g. do a year of evolution then write to file and overwrite the array
@@ -54,60 +70,33 @@ Diffusion = np.ones_like(Temp) * 0.2  # diffusion coefficient (Lat)
 
 for n in range(timedim):
     for m in range(spacedim):
-        if m == 0:
+        if m == 0 or m == 1:
             # forward difference for zero edge
-            # (T(x_m+2) - 2T(x_m+1) + T(x_m)) * D(x_m) * (1-x_m^2) / Δx^2
-            # + (T(x_m+1) - T(x_m) / Δx) * ((1-x_m^2) * (D(x_m+1) - D(x_m)) / Δx - 2*x_m*D(x_m))
-            # with m = 0 this reduces to:
-            diff_elem = (Temp[1, n] - Temp[0, n]) / dx * (2 * Diffusion[0, n])
-        elif m == 1:
-            # (T(x_m+2) - 2T(x_m+1) + T(x_m)) * D(x_m) * (1-x_m^2) / Δx^2
-            # + (T(x_m+1) - T(x_m) / Δx) * ((1-x_m^2) * (D(x_m+1) - D(x_m)) / Δx - 2*x_m*D(x_m))
-            # x_m = dx-1 i.e. one dx increment after the start at -1
-            diff_elem = (Temp[3, n] - 2 * Temp[2, n] + Temp[1, n]) * Diffusion[1, n] * (
-                1 - (dx - 1) ** 2
-            ) / dx**2 + ((Temp[2, n] - Temp[1, n]) / dx) * (
-                (1 - (dx - 1) ** 2) * (Diffusion[2, n] - Diffusion[1, n]) / dx
-                - 2 * (dx - 1) * Diffusion[1, n]
-            )
-        elif m == spacedim - 2:  # mirror of m == 1, ie m = -2 in python arrays
+            secondT = forward2ndorder(Temp[:, n], m, dx)
+            firstT = forwarddifference(Temp[:, n], m, dx)
+            firstD = forwarddifference(Diffusion[:, n], m, dx)
+        elif m == spacedim - 2 or m == spacedim - 1:
             # backwards difference for end edge
-            # (T(x_m) - 2*T(x_m-1) + T(x_m-2)) * D(x_m) * (1-x_m^2) / Δx^2
-            # + (T(x_m) - T(x_m-1)) / Δx * ((1-x_m^2) * (D(x_m) - D(x_m-1)) / Δx - 2*x_m*D(x_m))
-            # x_m = 1-dx i.e. one dx increment before the end at 1
-            diff_elem = (Temp[-2, n] - 2 * Temp[-3, n] + Temp[-4, n]) * Diffusion[
-                -2, n
-            ] * (1 - (1 - dx) ** 2) / dx**2 + ((Temp[-2, n] - Temp[-3, n]) / dx) * (
-                (1 - (1 - dx) ** 2) * (Diffusion[-2, n] - Diffusion[-3, n]) / dx
-                - 2 * (1 - dx) * Diffusion[-2, n]
-            )
-        elif m == spacedim - 1:  # mirror of m == 0 ie m = -1 in python arrays
-            # (T(x_m) - 2*T(x_m-1) + T(x_m-2)) * D(x_m) * (1-x_m^2) / Δx^2
-            # + (T(x_m) - T(x_m-1)) / Δx * ((1-x_m^2) * (D(x_m) - D(x_m-1)) / Δx - 2*x_m*D(x_m))
-            # x_m = 1
-            diff_elem = (Temp[-2, n] - Temp[-1, n]) / dx * (2 * Diffusion[-1, n])
+            secondT = backward2ndorder(Temp[:, n], m, dx)
+            firstT = backwarddifference(Temp[:, n], m, dx)
+            firstD = backwarddifference(Diffusion[:, n], m, dx)
         else:
-            # x_m = dx*m - 1 i.e. m steps along from -1
-            diff_elem = (Temp[m + 1, n] - 2 * Temp[m, n] + Temp[m - 1, n]) * Diffusion[
-                m, n
-            ] * (1 - (dx * m - 1) ** 2) / (4 * dx**2) + (
-                Temp[m + 1, n] - Temp[m - 1, n]
-            ) * (
-                (1 - (dx * m - 1) ** 2)
-                * (Diffusion[m + 1, n] - Diffusion[m - 1, n])
-                / (2 * dx)
-                - 2 * (dx * m - 1) * Diffusion[m, n]
-            ) / (
-                2 * dx
-            )
+            # central difference for most cases
+            secondT = central2ndorder(Temp[:, n], m, dx)
+            firstT = centraldifference(Temp[:, n], m, dx)
+            firstD = centraldifference(Diffusion[:, n], m, dx)
+        # diff = (dD/dx (1-x^2) + D*-2x)*dT/dx + D(1-x^2)* d^2(T)/ dx^2
+        diff_elem = (firstD * (1-xs[m]**2) - 2*Diffusion[m, n]*xs[m]) * firstT + Diffusion[m, n] * (1-xs[m]**2) * secondT
+        # T(x_m, t_n+1) = T(x_m, t_n) + Δt / C(x_m, t_n)
+        # * (diff - I + S(1-A) )
         Temp[m, n + 1] = Temp[m, n] + dt / Capacity[m, n] * (
             diff_elem - Ir_emission[m, n] + Source[m, n] * (1 - Albedo[m, n])
         )
 
 import matplotlib.pyplot as plt
 
-for n in range(10):
-    plt.plot(np.linspace(-1, 1, spacedim), Temp[:, 50 * n], label=f"t={50*n}")
+for n in range(0, 501, 50):
+    plt.plot(xs, Temp[:, n], label=f"t={n}")
 
 # plt.axvline(dx*20-1)
 plt.ylabel("Temperature")
