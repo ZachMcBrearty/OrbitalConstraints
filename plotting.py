700082beb898a5888bd99360fd44cf26bd8ec80d
ZachMcBrearty <mcbreartyzg@gmail.com>
2024-01-09 18:37:45 +0000

    moon specific plotting

--- plotting.py
+++ plotting.py
@@ -298,6 +298,72 @@ def semimajor_fit_plot(
     plt.show()
 
 
+def moon_semimajor_fit_plot(
+    tests: np.ndarray,
+    convtemps: np.ndarray,
+    val_name: str,
+    val_range,
+    val_unit: Optional[str] = None,
+    x_axis_scale: Literal["linear", "log"] = "linear",
+    y_axis_scale: Literal["linear", "log"] = "linear",
+):
+    if val_unit is None:
+        val_unit = ""
+    else:
+        val_unit = ", " + val_unit
+    # val_range = np.arange(val_min, val_max, val_step)
+    # fit_range = np.linspace(min(val_range), max(val_range), 100, endpoint=True)
+    # fit_vals = convtemps[0] * (1 - fit_range**2) ** (-1 / 4)
+    # fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig, (ax2, ax3) = plt.subplots(2, 1, height_ratios=[2, 1])
+    # ax1: plt.Axes
+    ax2: plt.Axes
+    ax3: plt.Axes
+    if min(convtemps) < 273:
+        ax2.axhline(273, 0, 1, ls="-.", label="273 K")
+    if max(convtemps) > 373:
+        ax2.axhline(373, 0, 1, ls="-.", label="373 K")
+    fit_xs = val_range[convtemps > 0]
+    fit_ys = convtemps[convtemps > 0]
+    tests[convtemps < 0] = np.nan
+    convtemps[convtemps < 0] = np.nan
+    # ax1.scatter(val_range, tests)
+    ax2.scatter(fit_xs, fit_ys, c="b", marker="x")
+
+    xs = np.linspace(fit_xs[0], fit_xs[-1], 100)
+
+    # fitter = lambda x, a, b, c: (a + c * x ** (-2) + b * x ** (-15 / 2)) ** (1 / 4)
+    fitter = lambda x, a, b: (a + b * x ** (-15 / 2)) ** (1 / 4)
+    print(fit_xs, fit_ys)
+    fits, pcov = curve_fit(fitter, fit_xs, fit_ys, absolute_sigma=True)
+    print(fits, np.sqrt(np.diag(pcov)))
+    ys = fitter(xs, *fits)
+    # fitter = lambda x, a: a * x ** (-1 / 2)
+    # a, pcov = curve_fit(fitter, fit_xs, fit_ys)
+    # print(a, np.sqrt(np.diag(pcov)))
+    # ys = fitter(xs, a)
+
+    ax2.plot(xs, ys, c="r", label="T = $(p + q\ a^{-15/2})^{1/4}$")
+    ax3.scatter(
+        fit_xs, fit_ys - fitter(fit_xs, *fits), label="T = $p_1$ a$^{-q_1}$ + r$_1$"
+    )
+
+    ax2.set_xlabel(f"{val_name} {val_unit}")
+    ax3.set_xlabel(f"{val_name} {val_unit}")
+    # ax1.set_xticks(np.linspace(min(val_range), max(val_range), 11))
+    # ax2.set_xticks(np.linspace(min(val_range), max(val_range), 11))
+    # ax1.set_xscale(x_axis_scale)
+    # ax1.set_yscale(y_axis_scale)
+    ax2.set_xscale(x_axis_scale)
+    ax3.set_xscale(x_axis_scale)
+    ax2.set_yscale(y_axis_scale)
+    # ax1.set_ylabel("Time to converge, years")
+    ax2.set_ylabel("Global convergent temperature, K")
+    ax3.set_ylabel("Residual, K")
+    ax2.legend()
+    plt.show()
+
+
 def convergence_plot_single_compare(
     tests_1,
     convtemps_1,
@@ -377,6 +443,53 @@ def ecc_fit_plot(
     plt.show()
 
 
+def moon_ecc_fit_plot(
+    tests,
+    convtemps,
+    val_name: str,
+    val_range,
+    val_unit: Optional[str] = None,
+):
+    val_range = np.array(val_range)
+    if val_unit is None:
+        val_unit = ""
+    else:
+        val_unit = ", " + val_unit
+
+    fit_xs = val_range[convtemps > 0]
+    fit_ys = convtemps[convtemps > 0]
+    xs = np.linspace(fit_xs[0], fit_xs[-1], 100)
+    # fitter = lambda x, a, b, c: (a * x**2 + b + c * (1 - x**2) ** (-1 / 2)) ** (
+    #     1 / 4
+    # )
+    fitter = lambda x, a, b: (b * x**2 + a) ** (1 / 4)
+    fits, pcov = curve_fit(fitter, fit_xs, fit_ys)
+    print(fits, np.sqrt(np.diag(pcov)))
+    ys = fitter(xs, *fits)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, height_ratios=[2, 1])
+    ax1: plt.Axes
+    ax2: plt.Axes
+    if min(convtemps) < 273:
+        ax1.axhline(273, 0, 1, ls="-.", label="273 K")
+    if max(convtemps) > 373:
+        ax1.axhline(373, 0, 1, ls="-.", label="373 K")
+    ax1.scatter(fit_xs, fit_ys, label="Model data")
+    ax1.plot(xs, ys, label="$T = (p + q\ e^2)^{1/4}$ fit")
+    ax2.scatter(
+        fit_xs,
+        (fit_ys - fitter(fit_xs, *fits)),
+    )
+    ax2.set_xlabel(f"{val_name} {val_unit}")
+    # ax1.set_xticks(val_range)
+    # ax2.set_xticks(val_range)
+    ax2.set_ylabel("Residual")
+    ax1.set_ylabel("Global convergent temperature, K")
+    ax1.legend()
+    # ax2.legend()
+    plt.show()
+
+
 def convergence_plot_dual(
     tests,
     convtemps,
@@ -407,6 +520,12 @@ def convergence_plot_dual(
     converge_temp_map = ax2.pcolormesh(
         val_1_range, val_2_range, convtemps.T, cmap="RdBu_r", shading="nearest"
     )
+
+    roche = 0.00042492578167103155
+    for i, semi in enumerate(val_1_range):
+        for j, ecc in enumerate(val_2_range):
+            if semi * (1 - ecc) < roche:
+                print("rochelimit!", i, j, ecc, semi)
 
     fig.colorbar(converge_time_map, ax=ax1, label="Time to converge, years")
     fig.colorbar(converge_temp_map, ax=ax2, label="Convergent Temperature, K")
@@ -626,7 +745,7 @@ if __name__ == "__main__":
 
     times, temps, degs = read_file("omega.npz")
     dt = times[1] - times[0]
-    colourplot(degs, temps, times, 90, None, 1)
+    # colourplot(degs, temps, times, 90, None, 1)
     # plotdata(degs, temps, dt, int(365 * 90.5), int(365 * 91.5), 12)
     # print(convergence_test(temps, rtol=0.0001))
     # yearavgplot(degs, temps, dt, 90, 120, 1)
